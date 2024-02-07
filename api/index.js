90ffa0c5252a10213a35b7221a6d96506511a5b2
gouravkshirsagar5500 <gouravkshirsagar5500@gmail.com>
2024-02-07 22:58:03 +0530

    Connect to DataBase

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
-import { Console } from 'console';
+import { Console, log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+//  this is for hideing data base  username and  password 
+//   if copyed this project  so  replace your mongodb data base(cluster url ) (mongodb+srv://gouravkshirsagar:<password>@nexthome.7tpi430.mongodb.net/?retryWrites=true&w=majority)   <-----> process.env.mongo 
+
+
+mongoose.connect( process.env.MONGO).then(()=>{
+    console.log('Connected to MONGOdb');
+}).catch((err)=>{
+    console.log(err);
+
+})
 
 const app = express();
 app.listen(3000,()=>{
