f143472548bf51081815538cb3d7857aa72d01b8
gouravkshirsagar5500 <gouravkshirsagar5500@gmail.com>
2024-02-10 16:34:59 +0530

    Create Signin api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, signin } from '../controllers/auth.controller.js';
 // don't forget to Add .js after importing user defind file. like in above file auth.controller.js //
 
 const router= express.Router();
 
 router.post("/signup", signup);
+router.post("/signin",signin)
+
 
 export default router;
 
