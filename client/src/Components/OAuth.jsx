ed3e30186c6846f63298656365b4fdbe170f0183
gouravkshirsagar5500 <gouravkshirsagar5500@gmail.com>
2024-02-14 16:35:29 +0530

    google complete OAuth

--- client/src/Components/OAuth.jsx
+++ client/src/Components/OAuth.jsx
@@ -3,9 +3,12 @@ import { app } from "../firebase";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import {  useDispatch } from "react-redux";
 import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
+
 
 export default function OAuth() {
   const dispatch = useDispatch();
+  const navigate =useNavigate();
 
   const handleGoogleClick = async () => {
     try {
@@ -27,6 +30,8 @@ export default function OAuth() {
       });
       const data = await res.json();
       dispatch(signInSuccess(data));
+      navigate("/");
+
     } catch (error) {
       console.log("Could not sign in with google", error);
     }
